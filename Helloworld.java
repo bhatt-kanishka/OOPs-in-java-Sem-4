f8b137a32da8100166e1cd4deb0e8a94a5645260
bhatt-kanishka <kanishkabhatt93@gmail.com>
2026-04-08 13:49:51 +0530

    Added Helloworld in git

--- Helloworld.java
+++ Helloworld.java
@@ -9,33 +9,3 @@ class Helloworld{
     }
 }
 
-/* 
-class Student{
-    String name;
-    int age;
-    String program;
-    float sgpa;
-
-    void display(){
-        System.out.println("Name of the student : "+name);
-         System.out.println("Name of the student : "+age);
-          System.out.println("Name of the student : "+program);
-           System.out.println("Name of the student : "+sgpa);
-           System.out.println("Name of the student : "+name);
-    
-}
-    public static void main(String[] args){
-        Student s1=new Student();
-         Student s2=new Student();
-
-        s1.name="kanishka";
-        s2.name="vb";
-        s1.age=19;
-        s1.program="Btech Cse";
-
-        s1.display();
-        s2.display();
-    }}
-    
-
-*/
